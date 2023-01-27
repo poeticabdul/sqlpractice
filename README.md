a09818b93527a4c84b497200c111a16462f2eb96
poeticabdul <poeticabdul@gmail.com>
2023-01-27 06:37:47 +0000

    first setup

---
+++ README.md
@@ -0,0 +1,1 @@
+### A directory showing my practice of SQL
---
+++ furniture_store.sql
@@ -0,0 +1,20 @@
+/*
+Furniture store sales data gotten from https://gist.github.com/pamelafox/791ce9e4eb31abce2362
+Inspired by Khan Academy course on SQL
+*/
+
+
+
+/* What are average, max, and min values in the data? */
+
+
+/* What about those numbers per category in the data (using HAVING)? */
+
+
+
+/* What ways are there to group the data values that don’t exist yet (using CASE)? */
+
+
+
+
+/* What interesting ways are there to filter the data (using AND/OR)? */
