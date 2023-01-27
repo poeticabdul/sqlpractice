c2d55c9779c5753905ad35c1948d81b2a9c408df
poeticabdul <poeticabdul@gmail.com>
2023-01-27 07:33:59 +0000

    Added queries using agg and having

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### A directory showing my practice of SQL
+
+#### Inspired by SQL course on Khan Academy
--- furniture_store.sql
+++ furniture_store.sql
@@ -1,20 +1,29 @@
 /*
 Furniture store sales data gotten from https://gist.github.com/pamelafox/791ce9e4eb31abce2362
-Inspired by Khan Academy course on SQL
+Inspired by Khan Academy course on SQL > https://www.khanacademy.org/computing/computer-programming/sql/more-advanced-sql-queries/pp/project-data-dig
 */
 
 
 
-/* What are average, max, and min values in the data? */
+/* === What are average, max, and min values in the data? === */
+
+SELECT AVG(price) FROM sales;
+SELECT MAX(price) FROM sales;
+SELECT MIN(price) FROM sales;
 
 
-/* What about those numbers per category in the data (using HAVING)? */
+/* === What about those numbers per category in the data (using HAVING)? === */
+
+/* Finding which countries have the minimum price that paid by Visa */
+SELECT country, MIN(price) AS minimum_price FROM sales GROUP BY country HAVING payment_type = "Visa";
+
+/* Find which products have a price of 3600 or less */
+SELECT product FROM sales GROUP BY product HAVING price <=3600;
+
+
+/* === What ways are there to group the data values that don’t exist yet (using CASE)? === */
 
 
 
-/* What ways are there to group the data values that don’t exist yet (using CASE)? */
 
-
-
-
-/* What interesting ways are there to filter the data (using AND/OR)? */
+/* === What interesting ways are there to filter the data (using AND/OR)? === */
