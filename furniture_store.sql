e4c31f4616fafa9c7d11b799d9aee84c5ce106b1
poeticabdul <poeticabdul@gmail.com>
2023-01-28 09:02:36 +0000

    added case, and/or quries

--- furniture_store.sql
+++ furniture_store.sql
@@ -1,5 +1,6 @@
 /*
 Furniture store sales data gotten from https://gist.github.com/pamelafox/791ce9e4eb31abce2362
+
 Inspired by Khan Academy course on SQL > https://www.khanacademy.org/computing/computer-programming/sql/more-advanced-sql-queries/pp/project-data-dig
 */
 
@@ -24,6 +25,27 @@ SELECT product FROM sales GROUP BY product HAVING price <=3600;
 /* === What ways are there to group the data values that don’t exist yet (using CASE)? === */
 
 
+/* Group cities and their countries by how much affordable they are using the price of furniture */
+SELECT city, country, price, 
+    CASE 
+        WHEN price >= 1200 AND price < 3600 THEN "affordable"
+        WHEN price >= 3600 AND price < 7200 THEN "moderately affordable"
+        WHEN price >= 7200 THEN "expensive"
+        ELSE "outlier"
+    END as "affordability"
+FROM sales;
 
 
 /* === What interesting ways are there to filter the data (using AND/OR)? === */
+
+/* Show the names of people, what type of furniture they purchased and their respective countries from UK or US */
+SELECT name, product, country FROM sales WHERE country = "United Kingdom" OR country = "United states";
+
+
+/* First show the names of countries with the name 'United' in them. Then show the names of people, what furniture type they purchased and their respective countries if their country name has 'United' in it. */
+SELECT country FROM sales WHERE country LIKE "%United%";
+
+SELECT name, product, country FROM sales WHERE country IN (SELECT country FROM sales WHERE country LIKE "%United%");
+
+
+
